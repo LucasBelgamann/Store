9a11945590b90cf665049989867b6671bab03050
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-03-14 21:13:18 -0300

    length span

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -3,15 +3,16 @@ import * as FaBeerIcon from "react-icons/fa";
 import * as BiBeerIcon from "react-icons/bi";
 import * as CgBeerIcon from "react-icons/cg";
 import * as AiBeerIcon from "react-icons/ai";
-import logo from './img/storeLogo.png';
+import logo from "./img/storeLogo.png";
 import { useContext } from "react";
 import Context from "../../context/Context";
-import './Nav.css';
+import "./Nav.css";
 import Searchbar from "./Search/Searchbar/Searchbar";
 import Cart from "./Search/Cart/Cart";
 
 export default function Navbar() {
-  const { isToggle, setIsToggle, isCart, setIisCart } = useContext(Context);
+  const { isToggle, setIsToggle, isCart, setIisCart, storage } =
+    useContext(Context);
 
   const handleToggle = () => {
     setIsToggle(!isToggle);
@@ -31,12 +32,16 @@ export default function Navbar() {
     <div className="navbar">
       <div className="nav-logo-link">
         <div className="logo-icon">
-          <img src={ logo } alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
         <div className="nav-links">
           <BsBeerIcon.BsPersonFill />
           <AiBeerIcon.AiFillHeart />
-          <FaBeerIcon.FaShoppingCart onClick={handleToggleCart} />
+          <FaBeerIcon.FaShoppingCart
+            onClick={handleToggleCart}
+          />
+          <span       className="cart"
+            data-count={storage.length}></span>
           <Cart />
         </div>
       </div>
@@ -44,11 +49,21 @@ export default function Navbar() {
         <div className="search">
           <BiBeerIcon.BiSearchAlt className="icon" onClick={handleToggle} />
           <div className="input">
-            <input type="text" onClick={handleToggle} placeholder="Buscar produtos" id="mysearch" />
+            <input
+              type="text"
+              onClick={handleToggle}
+              placeholder="Buscar produtos"
+              id="mysearch"
+            />
           </div>
-          <CgBeerIcon.CgClose className={!isToggle ? "clear" : "clear active-clear"} onClick={handleToggle} />
+          <CgBeerIcon.CgClose
+            className={!isToggle ? "clear" : "clear active-clear"}
+            onClick={handleToggle}
+          />
         </div>
-        <button type="button" className="btn-search">Buscar</button>
+        <button type="button" className="btn-search">
+          Buscar
+        </button>
       </div>
       <Searchbar />
     </div>
